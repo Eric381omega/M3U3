10b5b5ed202fac6d96718191a664626996b5445d
Eric381omega <ericgodzilladubs@gmail.com>
2024-09-23 12:15:09 -0300

    first commit

--- app.js
+++ app.js
@@ -5,11 +5,50 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 require('dotenv').config();
+var session = require('express-session');
 
 var indexRouter = require('./routes/index');
-//var usersRouter = require('./routes/users');
+var usersRouter = require('./routes/users');
+var loginRouter = require('./routes/admin/login');
+var adminRouter = require('./routes/admin/novedades');
 
 var app = express();
+
+//M3U4###
+
+const { title } = require('process');
+
+app.get('/ejemplo', function(req, res) {
+ if (req.session.nombre) {
+  res.send('Hola' + req.session.nombre);
+ } else {
+  res.send('Hola usuario desconocido.');
+ }
+});
+
+app.get('/', function(req, res) {
+  var conocido = Boolean(req.session.nombre);
+
+  res.render('index', {
+   title: 'Sesiones en Express.js',
+   conocido: conocido,
+   nombre: req.session.nombre
+  });
+});
+
+app.post('/ingresar', function(req, res) {
+ if (req.body.nombre) {
+  req.session.nombre = req.body.nombre
+ }
+ res.redirect('/')
+});
+
+app.get('/salir', function (req, res) {
+ req.session.destroy();
+ res.redirect('/');
+});
+
+//M3U4^^^
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,8 +60,29 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(session({
+  secret: '12w45qe4qleq54eq5',
+  resave: false,
+  saveUninitialized: true
+ }));
+
+ secured = async (req, res, next) => {
+  try {
+    console.log(req.session.id_usuario);
+    if (req.session.id_usuario) {
+      next();
+    } else {
+      res.redirect('admin/login')
+    }
+  } catch (error) {
+     console.log(error);
+  }
+ }
+
 app.use('/', indexRouter);
-//app.use('/users', usersRouter);
+app.use('/users', usersRouter);
+app.use('/admin/login', loginRouter);
+app.use('/admin/novedades', secured, adminRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
